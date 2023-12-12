f42c29259fb32d0f844e001ab2e029ce465fbe5b
Misha <mishagrigoryan@gmail.com>
2023-12-12 01:19:03 -0500

    changes

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask, jsonify
 import requests
 import config
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "NASA Data Dashboard Backend Running"
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
 @app.route('/apod')
 def apod():
-    response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key={config.NASA_API}')
+    response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key={config.NASA_API_KEY}')
     data = response.json()
     return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
