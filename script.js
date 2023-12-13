f3a67ff9b4f1ab0b57c24b6c487c879070ac0e79
Misha <mishagrigoryan@gmail.com>
2023-12-12 23:58:52 -0500

    ks

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('http://127.0.0.1:5000/apod')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('apod-image').src = data.url;
+            document.getElementById('apod-description').innerText = data.explanation;
+        })
+        .catch(error => console.error('Error:', error));
+});
