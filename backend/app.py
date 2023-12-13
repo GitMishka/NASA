072420630bedbe91b0ab3b8fb579a4798f0ef9fb
Misha <mishagrigoryan@gmail.com>
2023-12-13 00:17:34 -0500

    works

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import requests
 import config
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/')
 def home():
--- frontend/script.js
+++ frontend/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     fetch('http://127.0.0.1:5000/apod')
         .then(response => response.json())
         .then(data => {
-            document.getElementById('apod-image').src = data.url;
+            document.getElementById('apod-image').src = data.hdurl;  // or data.url
             document.getElementById('apod-description').innerText = data.explanation;
         })
         .catch(error => console.error('Error:', error));
