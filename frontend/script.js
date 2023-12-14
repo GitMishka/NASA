8e49d624e149953518b4d2a6519e8b797051954a
Misha <mishagrigoryan@gmail.com>
2023-12-13 23:58:20 -0500

    rewamp

--- frontend/script.js
+++ frontend/script.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     fetch('http://127.0.0.1:5000/apod')
         .then(response => response.json())
         .then(data => {
-            document.getElementById('apod-image').src = data.hdurl;  // or data.url
+            document.getElementById('apod-image').src = data.hdurl;
+            document.getElementById('apod-title').innerText = data.title;
             document.getElementById('apod-description').innerText = data.explanation;
         })
         .catch(error => console.error('Error:', error));
